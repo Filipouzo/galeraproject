8f9a0a4c179d864ef7d8233bb79af1c0a622ca91
Filipouzo <philiparna@gmail.com>
2023-11-15 10:48:34 +0100

    Stop sequelize.sync

--- db.config.js
+++ db.config.js
@@ -1,8 +1,8 @@
-/** Import module */
+/* Import module */
 const { Sequelize } = require('sequelize')
 
 
-/** Connexion à la base de donnée */
+/* Connexion à la base de donnée */
 
 let sequelize = new Sequelize(
     process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
@@ -14,7 +14,7 @@ let sequelize = new Sequelize(
 )
 
 
-/** Mise en place des modèles et relation */
+/* Mise en place des modèles et relation */
 const db = {}
 
 db.sequelize = sequelize
@@ -29,7 +29,8 @@ db.Formation.hasMany(db.Eleve, {foreignKey: 'id_formation'})
 db.Eleve.belongsTo(db.Formation, {foreignKey: 'id_formation'})
 
 
-/** Synchronisation des modèles */
-db.sequelize.sync({alter: true})
+/* Synchronisation des modèles */
+/* Ma base de donnée est stable et j'ai fini de connecté mes modèles je peux commenter la ligne suivante  */
+/* db.sequelize.sync({alter: true}) */
 
 module.exports = db
