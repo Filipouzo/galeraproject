88a59534f5e0828c07c983fcaa0dfd3285b13b3e
Filipouzo <philiparna@gmail.com>
2023-11-18 09:22:45 +0100

    Add Eleve Model

--- Models/Eleve.js
+++ Models/Eleve.js
@@ -31,7 +31,7 @@ module.exports = (sequelize) => {
         },
         password:{
             type: DataTypes.STRING(64),
-            is: /^[0-9a-f]{64}$/i    
+            is: /^[0-9a-f]{5,}$/i
         }
     })
 
